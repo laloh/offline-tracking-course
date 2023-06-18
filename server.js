1c9db40d36f3e397df22c6d555cdfe18e76ef336
laloh <eduardocarhue@gmail.com>
2023-06-18 15:43:38 -0600

    Init db

--- server.js
+++ server.js
@@ -1,53 +1,33 @@
 import express from 'express';
-import sqlite3 from 'sqlite3';
+import bodyParser from 'body-parser';
+import db from './database.js';
 
 // App Config
 const app = express();
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 8001;
 
 // Middlewares
 
-// DB Config
-const DBSOURCE = 'database.sqlite';
-const db = new sqlite3.Database(DBSOURCE, (err) => {
-    if (err) {
-        // Cannot open database
-        console.error(err.message);
-        throw err;
-    } else {
-        console.log("Connected to the SQLite database.")
-        // Create table
-        db.run(`CREATE TABLE courses (
-                    id INT PRIMARY KEY,
-                    name VARCHAR(255) NOT NULL,
-                    description VARCHAR(255) NOT NULL,
-                    created_at TIMESTAMP,
-                    updated_at TIMESTAMP)`,
-            (err) => {
-                if (err) {
-                   console.log("Table already created.");
-                } 
-            });
-        
-        db.run(`CREATE TABLE videos(
-                    id INT PRIMARY KEY,
-                    title VARCHAR(255) NOT NULL,
-                    url VARCHAR(255) NOT NULL,
-                    course_id INT NOT NULL,
-                    FOREIGN KEY (course_id) REFERENCES courses(id))`,
-            (err) => {
-                if (err) {
-                    console.log("Table already created.");
-                } 
-            });
-    };
+// API Endpoints
+app.get('/api/courses', (req, res) => {
+    res.status(200).send('Hello World');
 });
 
-// Course Reader
+app.post('/api/courses', (req, res) => {
+    const { name, path, description } = req.body;
+    const createdAt = new Date();
+    const updatedAt = new Date();
+    
+    const sql = `INSERT INTO courses (name, path, description, created_at, updated_at) VALUES (?, ?, ?, ?, ?)`;
+    const params = [name, path, description, createdAt, updatedAt];
+    
+    db.run(sql, params);
 
+    res.status(201).send('Hello World');
+});
 
-// API Endpoints
-app.get('/', (req, res) => res.status(200).send('Hello World'));
 
 // Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
