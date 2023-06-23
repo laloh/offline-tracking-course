2e5b028dc2d8072acc4d618fc1359f7553227642
laloh <eduardocarhue@gmail.com>
2023-06-22 23:54:46 -0600

    Fix class to className, update checkbox id and replace class with className in SideMenu.jsx

--- frontend/app/components/SideMenu.jsx
+++ frontend/app/components/SideMenu.jsx
@@ -37,7 +37,7 @@ export default function SideMenu({ courseId }) {
             <li key={index}>
               <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
                 <input
-                  id="bordered-checkbox-1"
+                  id={`checkbox-${index}`}
                   type="checkbox"
                   className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
@@ -45,7 +45,7 @@ export default function SideMenu({ courseId }) {
                   htmlFor="bordered-checkbox-1"
                   className="w-full py-2 ml-1 text-sm font-medium text-gray-900 dark:text-black-300"
                 >
-                  <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">{video.title}</a>
+                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">{video.title}</a>
 
                 </label>
               </div>
